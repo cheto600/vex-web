5edce31e0624c796e7ddcd7f3620c84dbfdb98f6
VEX <you@example.com>
2026-06-26 01:32:38 +0200

    Update hero background video

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import FadeIn from './FadeIn'
 import WohnNowLogo from './WohnNowLogo'
 
 const VIDEO_URL =
-  'https://d8j0ntlcm91z4.cloudfront.net/user_38xzZboKViGWJOttwIXH07lWA1P/hf_20260403_050628_c4e32401-fab4-4a27-b7a8-6e9291cd5959.mp4'
+  'https://d8j0ntlcm91z4.cloudfront.net/user_38xzZboKViGWJOttwIXH07lWA1P/hf_20260405_171521_25968ba2-b594-4b32-aab7-f6b69398a6fa.mp4'
 
 export default function Hero() {
   return (
